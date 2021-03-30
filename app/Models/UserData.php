29f87ccdaafee37272e2ecb2e69251cda606f89c
JeremyZunino <arwing64>
2021-03-30 19:28:49 +0200

    Préparation du controller UserData

--- app/Models/UserData.php
+++ app/Models/UserData.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserData extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'birthday'
+    ];
 }
