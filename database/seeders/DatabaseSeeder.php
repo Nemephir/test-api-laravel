52601c96f2ff01767a95fc9aa25ef5c352cb5715
Nemephir <Triforce-64>
2021-04-05 13:35:12 +0200

    Utilisateurs : ajout colonne api_token

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\UserData::factory(10)->create();
-        // $this->call( UserDataTableSeeder::class );
-        // \App\Models\User::factory(10)->create();
     }
 }
